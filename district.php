9f22bdb83355fd78b282ec6f560ab61a42deacc6
roy <aroy.sourov@gmail.com>
2022-04-26 01:57:04 +0600

    thana also done

---
+++ district.php
@@ -0,0 +1,14 @@
+<?php
+require_once 'database.php';
+
+$id = $_GET['id'];
+$dis_query = "SELECT * FROM tbl_district where division_id = $id";
+$result2 = mysqli_query($conn, $dis_query);
+?>
+ <select >
+    <option value="0">--- Select District ---</option>
+    <?php foreach ($result2 as $row_sl2) { ?>
+        <option value="<?php echo $row_sl2['id']; ?>"><?php echo $row_sl2['name']; ?></option>             
+    <?php } ?>
+</select>
+
